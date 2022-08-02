718b73a41e16820194370eed6f1d5447e76886fd
AsutinWeightRotate <austin.weight@studiorotate.com>
2022-08-02 09:07:13 +0100

    update config

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -6,10 +6,10 @@ export default defineConfig({
     defaultLanguageCode: 'EN',
     storeDomain:
       // @ts-ignore
-      process?.env?.SHOPIFY_STORE_DOMAIN || 'hydrogen-preview.myshopify.com',
+      process.env?.SHOPIFY_STORE_DOMAIN || 'hydrogen-preview.myshopify.com',
     storefrontToken:
       // @ts-ignore
-      process?.env?.SHOPIFY_STOREFRONT_API_PUBLIC_TOKEN ||
+      process.env?.SHOPIFY_STOREFRONT_API_PUBLIC_TOKEN ||
       '3b580e70970c4528da70c98e097c2fa0',
     storefrontApiVersion: '2022-07',
   },
