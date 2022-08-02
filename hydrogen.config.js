077a530058519cd2a4c6dc89f9fbe11630385245
AsutinWeightRotate <austin.weight@studiorotate.com>
2022-08-02 08:55:20 +0100

    update config

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -1,18 +1,29 @@
 import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
+import MyServerAnalyticsConnector from './MyServerAnalyticsConnector.jsx';
 
 export default defineConfig({
-  shopify: {
+  shopify: () => ({
     defaultCountryCode: 'US',
     defaultLanguageCode: 'EN',
-    storeDomain: 'hydrogen-preview.myshopify.com',
-    storefrontToken: '3b580e70970c4528da70c98e097c2fa0',
+    storeDomain:
+      // @ts-ignore
+      Oxygen?.env?.SHOPIFY_STORE_DOMAIN || 'rotate-test.myshopify.com',
+    storefrontToken:
+      // @ts-ignore
+      Oxygen?.env?.SHOPIFY_STOREFRONT_API_PUBLIC_TOKEN ||
+      'aabcd35cccd09b9e5cb247a9f242414a',
     storefrontApiVersion: '2022-07',
-  },
+  }),
   session: CookieSessionStorage('__session', {
     path: '/',
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    secure: import.meta.env.PROD,
     sameSite: 'Strict',
     maxAge: 60 * 60 * 24 * 30,
   }),
+  serverAnalyticsConnectors: [MyServerAnalyticsConnector],
 });
+
+// a0603620c521844359317831d9bde769
+
+// 36170efe08be8dd7dcdd9967ed0a102b
